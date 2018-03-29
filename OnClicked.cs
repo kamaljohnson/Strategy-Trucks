52e1af03fcb0f551086d87e2fb12afb76de284d5
Kamal Johnson <kamaljohnson12345@gmail.com>
2018-03-29 22:54:16 +0530

    Add files via upload

---
+++ OnClicked.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClicked : MonoBehaviour {
+
+	playerController control;
+	public GameObject OnClickUI;
+
+	void Start()
+	{
+		control = GetComponent<playerController>();
+	}
+	void OnMouseDown()
+	{
+		OnClickUI.SetActive(true);
+	}
+	public void moveSelect()
+	{
+		control.selected = true;
+		OnClickUI.SetActive(false);
+	}
+	public void actionSelect()
+	{
+
+	}
+}
--- playerController.cs
+++ playerController.cs
@@ -8,6 +8,9 @@ public class playerController : MonoBehaviour {
 	public LayerMask movementMask;
 	playerMotor motor;
 	Camera cam;
+	public bool publicPlayer;
+	public bool selected;	//this will be true if the player is selected
+	Vector3 destination;
 	// Use this for initialization
 	void Start () {
 		cam = Camera.main;
@@ -16,15 +19,23 @@ public class playerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0))
+		if(selected)
 		{
-			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			Debug.Log("here");
-			if(Physics.Raycast(ray, out hit))
+			if(Input.GetMouseButtonDown(0))
 			{
-				Debug.Log("hit " + hit.collider.name + " " + hit.point);
-				motor.MoveToPoint(hit.point);
+				Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				Debug.Log("here");
+				if(Physics.Raycast(ray, out hit))
+				{
+					destination = hit.point;
+					Debug.Log("hit " + hit.collider.name + " " + destination);
+					if (hit.collider.name != GetComponent<Collider>().name)
+					{
+						motor.MoveToPoint(destination);
+						selected = false;
+					}
+				}
 			}
 		}
 	}
